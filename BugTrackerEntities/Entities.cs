cdda167cc854c4e3fbc624cfd86023de34234c12
Giovanni Solinas <giovanni.solinas98@gmail.com>
2020-12-19 17:04:26 +0100

    Svolta prima parte del laboratorio

--- BugTrackerEntities/Entities.cs
+++ BugTrackerEntities/Entities.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -8,4 +9,64 @@ using System.Threading.Tasks;
 
 namespace BugTrackerEntities
 {
+    public class BugTrackerContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public int ZipCode { get; set; }
+    }
+
+    public class User
+    {
+        [Key]   //superfluo perchè UserId segue già la convenzione
+        public int UserId { get; set; }
+        [Required]
+        [MinLength(8)]
+        [MaxLength(32)]
+        [Index(IsUnique = true)]
+        public string Login { get; set; }
+        [Required]
+        [MinLength(16)]
+        //Cryptare la password
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [MinLength(16)]
+        [MaxLength(16)]
+        [Index(IsUnique = true)]
+        public string FiscalCode { get; set; }
+        public DateTime BirthDate { get; set; }
+        [NotMapped]
+        public int Age { get { return System.DateTime.Now.Year - BirthDate.Year; } }
+        public Address Address { get; set; }
+    }
+
+    public class Product
+    {
+        [Key] //Serve [Key] perchè code non segue la convenzione
+        public int Code { get; set; }
+        [Required]
+        public string CommercialName { get; set; }
+        [Required]
+        public string InternalName { get; set; }
+        [MaxLength(256)]
+        public string Description { get; set; }
+        [InverseProperty(nameof(Depended))]
+        public virtual  ICollection<Product> DependsOn { get; set; }
+        [InverseProperty(nameof(DependsOn))]
+        public virtual ICollection<Product> Depended { get; set; }
+        [InverseProperty(nameof(IncompatibleWith1))]
+        public virtual ICollection<Product> IncopatibleWith { get; set; }
+        [InverseProperty(nameof(IncopatibleWith))]
+        public virtual ICollection<Product> IncompatibleWith1 { get; set; }
+    }
 }
--- BugTrackerUI/Program.cs
+++ BugTrackerUI/Program.cs
@@ -11,7 +11,50 @@ namespace BugTrackerUI
     {
         static void Main(string[] args)
         {
-            
+            using (var  c = new BugTrackerContext())
+            {
+                c.Database.Delete();
+                c.Database.Create();
+            }
+            using (var c =  new BugTrackerContext())
+            {
+                var user = c.Users.Create();
+                user.Login = "GiovanniSolinas";
+                user.FirstName = "Giovanni";
+                user.LastName = "Solinas";
+                user.Password = "kbsajbsakjaksbcscscvwcw";
+                user.BirthDate = new DateTime(1998, 9,11);
+                user.Address = new Address() {ZipCode = 16100};
+                c.Users.Add(user);
+                c.SaveChanges();
+            }
+            using (var c = new BugTrackerContext())
+            {
+                var product = c.Products.Create();
+                product.Code = 42;
+                product.CommercialName = "Motore";
+                product.InternalName = "Motore 1.5 95 cv";
+                product.Description = "abajdcowjdbcjdbcwjbcw";
+                c.Products.Add(product);
+                c.SaveChanges();
+            }
+            using (var c = new BugTrackerContext())
+            {
+                var product = c.Products.Create();
+                product.Code = 27;
+                product.CommercialName = "Matita";
+                product.InternalName = "Matita bianca";
+                product.Description = "oojosjcbdwjcoswcojbwe";
+                c.Products.Add(product);
+                c.SaveChanges();
+            }
+            using (var c = new BugTrackerContext())
+            {
+                var product1 = c.Products.FirstOrDefault(p => p.Code == 1);
+                var product2 = c.Products.FirstOrDefault(p => p.Code == 2);
+                product1.DependsOn.Add(product2);
+                c.SaveChanges();
+            }
         }
     }
 }
